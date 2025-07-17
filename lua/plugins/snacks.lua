69c65ce0b5db0445fce888c74cd1f387207be601
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-07-17 14:59:07 +0200

    snacks smart picker

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -87,7 +87,8 @@ return {
 			-- find
 			-- { "<leader>fb",      function() Snacks.picker.buffers() end,                                 desc = "Buffers" },
 			-- { "<leader>fc",      function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
-			{ "<leader><space>",  function() Snacks.picker.files() end,                 desc = "Find Files" },
+			-- { "<leader><space>",  function() Snacks.picker.files() end,                 desc = "Find Files" },
+			{ "<leader><space>",  function() Snacks.picker.smart() end,                 desc = "Find Files" },
 			{ "<leader>sf",       function() Snacks.picker.files() end,                 desc = "Find Files" },
 			-- { "<leader>fg",      function() Snacks.picker.git_files() end,                               desc = "Find Git Files" },
 			-- { "<leader>fp",      function() Snacks.picker.projects() end,                                desc = "Projects" },
