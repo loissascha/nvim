13f5c2a98fa7a0c2f986b25324f47a1fb2e0e26b
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-10-31 08:36:47 +0100

    err != nil block

--- init.lua
+++ init.lua
@@ -116,6 +116,8 @@ vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" }
 -- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 -- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
+vim.keymap.set('n', '<leader>ee', "oif err != nil {<CR>}<Esc>Oreturn err<Esc>", { desc = "if err != nil" })
+
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
