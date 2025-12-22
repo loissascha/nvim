66a979b32bbb79dab78a32ae9a7a144f9f46222e
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-12-22 13:13:18 +0100

    inline diagnostics

---
+++ lua/plugins/tiny-inline-diagnostic.lua
@@ -0,0 +1,20 @@
+return {
+	{
+		"rachartier/tiny-inline-diagnostic.nvim",
+		event = "VeryLazy",
+		priority = 1000,
+		config = function()
+			require("tiny-inline-diagnostic").setup({
+				options = {
+					add_messages = {
+						display_count = true,
+					},
+					multilines = {
+						enabled = true,
+					}
+				}
+			})
+			-- vim.diagnostics.config({ virtual_text = false })
+		end
+	},
+}
