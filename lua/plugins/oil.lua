04fa880fc4cb0b462f20230f3ad31d05f2b95725
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-02-17 07:27:54 +0100

    activated oil; updated snacks to not use smart picker

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,11 +1,17 @@
 return {
-	-- {
-	-- 	"stevearc/oil.nvim",
-	-- 	---@module 'oil'
-	-- 	---@type oil.SetupOpts
-	-- 	opts = {},
-	-- 	-- Optional dependencies
-	-- 	dependencies = { { "echasnovski/mini.icons", opts = {} } },
-	-- 	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
-	-- },
+	{
+		"stevearc/oil.nvim",
+		---@module 'oil'
+		---@type oil.SetupOpts
+		opts = {},
+		-- Optional dependencies
+		dependencies = { { "echasnovski/mini.icons", opts = {} } },
+		-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+		lazy = false,
+		config = function()
+			require("oil").setup({
+			})
+			vim.keymap.set("n", "-", ":Oil<CR>", { desc = "Open parent directory" })
+		end,
+	},
 }
