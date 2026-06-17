2a22c01715b949a2a0bbf4f04fdd23d1f446759a
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-06-17 19:03:34 +0200

    fix auto import code action

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -117,10 +117,23 @@ return {
 						vim.api.nvim_create_autocmd("BufWritePre", {
 							buffer = event.buf,
 							callback = function()
-								vim.lsp.buf.code_action({
-									context = { only = { "source.organizeImports" } },
-									apply = true,
-								})
+								local params = vim.lsp.util.make_range_params(0, "utf-8")
+								params.context = { only = { "source.organizeImports" } }
+
+								local result = vim.lsp.buf_request_sync(event.buf, "textDocument/codeAction", params, 1000)
+								for _, res in pairs(result or {}) do
+									for _, action in pairs(res.result or {}) do
+										if action.edit then
+											vim.lsp.util.apply_workspace_edit(action.edit, "utf-8")
+										end
+
+										if action.command then
+											vim.lsp.buf.execute_command(action.command)
+										end
+
+										return
+									end
+								end
 							end,
 						})
 					end
